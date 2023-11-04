caec6fa5dfa66c960322d1879d7a073930075837
Lifecloud-qr <office@lifecloud-qr.com>
2023-11-04 15:56:01 +0200

    abusive post url not required

--- client/src/components/AbuseProfileForm/AbuseProfileForm.jsx
+++ client/src/components/AbuseProfileForm/AbuseProfileForm.jsx
@@ -300,7 +300,6 @@ export default function AbuseProfileForm() {
           <input
             className="form-input"
             {...register("linkToPost", {
-              required: true,
               pattern:
                 /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
             })}
