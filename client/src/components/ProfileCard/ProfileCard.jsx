9b646bf2d2722021593a3334393640ddc845843f
Lifecloud-qr <office@lifecloud-qr.com>
2023-11-19 13:45:29 +0200

    profile-page

--- client/src/components/ProfileCard/ProfileCard.jsx
+++ client/src/components/ProfileCard/ProfileCard.jsx
@@ -12,6 +12,19 @@ function FoundResults() {
     setReports(location.state.reports);
   }, [location.state.reports]);
 
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // January is 0!
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+
+  function extractAfterSlash(url) {
+    const parts = url.split("/");
+    return parts[parts.length - 1]; // returns the last element
+  }
+
   return (
     <div className="report-card-container">
       {/* The search bar is commented out, but I'm leaving it in case you want to use it later */}
@@ -32,40 +45,83 @@ function FoundResults() {
 
       <h1 className="home__title">{location?.state?.profileLink}</h1>
       <div>
-          <div
-            className="half-arc"
-            style={{ "--percentage": `${reports.length/2}%` }}
-          >
-            <span className="label">Danger level:
-            <br/>
-             {reports.length} Report
-            <br/>
-              {reports.length < 10 ? "low" : reports.length < 25 ? "medium" : "High"}
-             </span>
+        <div className="information-reputation-container">
+          <div className="profile-information">
+            <h6 className="mb-3">Profile information</h6>
+            <hr />
+            <div className="mt-3 mb-3">
+              <span>Info #1</span>
+              <span className="text-white ml-20">Text Here</span>
+            </div>
+            <div className="mb-3">
+              <span>Info #2</span>
+              <span className="text-white ml-20">Text Here</span>
+            </div>
+            <div className="mb-3">
+              <span>Info #3</span>
+              <span className="text-white ml-20">Text Here</span>
+            </div>
+            <div className="mb-3">
+              <span>Info #4</span>
+              <span className="text-white ml-20">Text Here</span>
+            </div>
           </div>
+          <div className="reputation">
+            <h6>Reputation level</h6>
+            <hr />
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <h1 className="mt-3 mb-3">Recent Reports ({reports.length})</h1>
+          <div className="reports-container">
+            <div className="grey-row rounded-t-2xl">
+              <span className="w-44 inline-block">Reporter</span>
+              <span className="w-32 inline-block">Date</span>
+              <span className="w-80 inline-block">Comment</span>
+              <span className="w-80 inline-block">Categories</span>
+              <span className="w-5 inline-block"></span>
+            </div>
+            {reports.length > 0 ? (
+              reports.map((report, index) => (
+                <div key={index} className="h-14 flex overflow-hidden p-3 row">
+                  {console.log(report)}
+                  <span className="w-44 inline-block overflow-hidden mr-1">
+                    {extractAfterSlash(report.abuseProfile.profileLink)}
+                  </span>
+                  <span className="w-32 inline-block overflow-hidden mr-1">
+                    {formatDate(report.abuseProfile.updatedAt)}
+                  </span>
+                  <span className="w-80 inline-block overflow-hidden mr-1">
+                    {report.comment}
+                  </span>
+                  <span className="w-80 inline-block overflow-hidden mr-1">
+                    {report.categories.map(
+                      (category, index) => `${category}, `
+                    )}
+                  </span>
+                  <a
+                    className="w5 inline-block overflow-hidden mr-1"
+                    href={report.linkToPost}
+                  >
+                    To
+                  </a>
+                </div>
+              ))
+            ) : (
+              <p className="home__no-reports">No reports found.</p>
+            )}
+          </div>
+        </div>
       </div>
-      {reports.length > 0 ? (
-        reports.map((report, index) => (
-          <div className="report-card" key={index}>
-            <h3 className="report-card__heading">Categories</h3>
-            <ul className="report-card__categories">
-              {report.categories.map((category, index) => (
-                <li key={index} className="report-card__category">
-                  {category}
-                </li>
-              ))}
-            </ul>
-            <h3 className="report-card__heading">Comment</h3>
-            <p className="report-card__comment">{report.comment}</p>
-            <h3 className="report-card__heading">Link to Abusive Post</h3>
-            <a href={report.linkToPost} className="report-card__link">
-              {report.linkToPost}
-            </a>
-          </div>
-        ))
-      ) : (
-        <p className="home__no-reports">No reports found.</p>
-      )}
+      <div className="profile-claim mt-3 p-3 flex flex-col items-center">
+        <h5 className="text-white mb-1">Is this your profile?</h5>
+        <h6 className="mb-2">
+          You may request to takedown any associated reports. We will attempt to
+          verify your ownership.
+        </h6>
+        <div className="verify-button cursor-pointer">Verify ownership</div>
+      </div>
     </div>
   );
 }
--- server/controllers/reportsController.js
+++ server/controllers/reportsController.js
@@ -3,7 +3,7 @@ const handlerFactory = require('./handlerFactory');
 
 exports.createReport = handlerFactory.createOne(Report);
 exports.updateReport = handlerFactory.updateOne(Report);
-exports.getAllReports = handlerFactory.getAll(Report);
+exports.getAllReports = handlerFactory.getAll(Report, 'abuseProfile');
 exports.getRecentReports = handlerFactory.getRecent(Report);
 exports.getReport = handlerFactory.getOne(Report);
 exports.deleteReport = handlerFactory.deleteOne(Report);
